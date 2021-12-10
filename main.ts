c7eb972bc1db0df642e2b88c5f29e8ab6a4b55ab
jay920319 <74820588+jay920319@users.noreply.github.com>
2021-12-10 13:15:54 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -287,22 +287,24 @@ scene.setBackgroundImage(img`
     `)
 info.setLife(5)
 boss = sprites.create(img`
-    . . f f f . . . . . . . . . . . 
-    f f f c c . . . . . . . . f f f 
-    f f c c . . c c . . . f c b b c 
-    f f c 3 c c 3 c c f f b b b c . 
-    f f b 3 b c 3 b c f b b c c c . 
-    . c b b b b b b c f b c b c c . 
-    . c b b b b b b c b b c b b c . 
-    c b 1 b b b 1 b b b c c c b c . 
-    c b b b b b b b b c c c c c . . 
-    f b c b b b c b b b b f c . . . 
-    f b 1 f f f 1 b b b b f c c . . 
-    . f b b b b b b b b c f . . . . 
-    . . f b b b b b b c f . . . . . 
-    . . . f f f f f f f . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    ..fff.............
+    fffcc........fff..
+    ffcc..cc...fcbbcff
+    ffc3cc3ccffbbbcccc
+    ffb3bc3bcfbbcccbc.
+    ccbbbbbbcfbcbccbc.
+    ccbbbbbbcbbcbbcbc.
+    cbbbbbbbbbcccbcbc.
+    cbbbbbbbbcccccb...
+    bb1bbb1bbbbbbc....
+    bbbbbbbbbbbbcc....
+    bbcbbbcbbbbccc....
+    fb1fff1bbbccccc...
+    fbbbbbbbbbbbfc....
+    .fbbbbbbbbbbfcc...
+    ..fbbbbbbbbcf.....
+    ...fbbbbbbcf......
+    ....fffffff.......
     `, SpriteKind.Enemy)
 boss.setPosition(142, 54)
 let statusbar = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
@@ -366,7 +368,9 @@ forever(function () {
         for (let value of boss_bullet) {
             if (value.overlapsWith(hero)) {
                 value.destroy()
-                info.setLife(info.life() - 1)
+                timer.throttle("action", 1000, function () {
+                    info.setLife(info.life() - 1)
+                })
                 if (info.life() <= 0) {
                     hero.destroy()
                 }
